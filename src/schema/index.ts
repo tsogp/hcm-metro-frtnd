32ab0a2670b8e113fd1cf18aaf7c0125dacf1c7a
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-04-18 18:07:27 +0700

    refactor: separate types, schema and utils folders

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// --------REGISTER SCHEMA--------
 export const step1Schema = z
   .object({
     email: z
@@ -76,3 +77,46 @@ export const step3Schema = z.object({
 export type Step1Values = z.infer<typeof step1Schema>;
 export type Step2Values = z.infer<typeof step2Schema>;
 export type Step3Values = z.infer<typeof step3Schema>;
+
+// --------LOGIN SCHEMA --------
+export const loginFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email format" })
+    .refine(
+      (email) => {
+        const domain = email.split(".").pop()?.toLowerCase();
+        return domain === "com" || domain === "vn";
+      },
+      {
+        message: "Email must end with .com or .vn",
+      }
+    )
+    .refine((email) => !/[\s<>()[\]\\,;:{}|^~`]/.test(email), {
+      message: "Email cannot contain spaces or special characters",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, {
+      message: "Password must be at least 8 characters long",
+    })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/\d/, {
+      message: "Password must contain at least one digit",
+    })
+    .regex(/[@#$%]/, {
+      message:
+        "Password must contain at least one special character (@, #, $, %)",
+    }),
+  remember: z.boolean().optional(),
+});
+
+export type LoginFormValues = z.infer<typeof loginFormSchema>;
