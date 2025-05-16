9bbcfced89217f935bfece069d993f2788a8691a
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-05-16 12:29:09 +0700

    add: implement stations and ticket type API

---
+++ src/action/stations.ts
@@ -0,0 +1,29 @@
+import { Station } from "@/types/station";
+import API from "@/utils/axiosClient";
+
+
+
+export const getAllStations = async (): Promise<Station[]> => {
+  try {
+    const response = await API.get("/stations", {
+      withCredentials: true,
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch stations:", error);
+    throw error;
+  }
+};
+
+export const getStationById = async (stationId: string): Promise<Station> => {
+  try {
+    const response = await API.get(`/stations/${stationId}`, {
+      withCredentials: true,
+    });
+    return response.data.data;
+
+  } catch (error) {
+    console.error("Failed to fetch station by id:", error);
+    throw error;
+  }
+};
---
+++ src/action/ticket.ts
@@ -0,0 +1,55 @@
+import API from "@/utils/axiosClient";
+
+interface Ticket {
+  ticketType: string;
+  typeName: string;
+  price: number;
+  expiryDescription: string;
+  requirementDescription: string;
+  expiryInterval: {
+    seconds: number;
+    zero: boolean;
+    nano: number;
+    negative: boolean;
+    positive: boolean;
+    units: [
+      {
+        durationEstimated: boolean;
+        timeBased: boolean;
+        dateBased: boolean;
+      }
+    ];
+  };
+  active: boolean;
+}
+
+export const getAllTicketTypes = async (metroLineId: string): Promise<Ticket[]> => {
+  try {
+    const response = await API.get("/ticket/ticket-type", {
+      params: {
+        metroLineId,
+      },
+      withCredentials: true,
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch tickets:", error);
+    throw error;
+  }
+};
+
+export const getBestTicketTypes = async (passengerId: string, email: string): Promise<Ticket[]> => {
+  try {
+    const response = await API.get("/ticket/best-ticket-type", {
+      params: {
+        passengerId,
+        email,
+      },
+      withCredentials: true,
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch best ticket type:", error);
+    throw error;
+  }
+};
